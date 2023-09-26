0876085ad20f669d2bff255f1a8068d2e1fccc13
GiselleP <pachecog2706@gmail.com>
2023-09-26 17:38:50 -0600

    exercise 2.8 done!

--- ex-2/src/App.js
+++ ex-2/src/App.js
@@ -2,10 +2,12 @@ import { useState } from 'react'
 
 
 const App = () => {
+
   const [names, setName] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas' , number: '39-44-5323523' }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumbers, setNewNumbers] = useState('')
 
 
 
@@ -13,16 +15,21 @@ const App = () => {
     event.preventDefault()
     const contactObject = {
       name: newName,
+      number:newNumbers,
       id: names.length+1,
     }
     setName(names.concat(contactObject))
     setNewName('')
+    setNewNumbers('')
   }
-
 
 
   const handleNamesChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumbersChange = (event) => {
+    setNewNumbers(event.target.value)
   }
 
   if (names.some((contact) => contact.name === newName)) {
@@ -35,9 +42,8 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addContact}>
-        <div>
-          name:  <input value={newName} onChange={handleNamesChange} /> 
-          </div>
+        <div> name:  <input value={newName} onChange={handleNamesChange} /> </div>
+         <div> number:  <input value={newNumbers} onChange={handleNumbersChange} /> </div>
         <div>
           <button type="submit">add</button>
         </div>
@@ -46,9 +52,9 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {names.map((contact) => (
-          <li key={contact.id}>{contact.name}</li>
+          <li key={contact.id}>{contact.name} {contact.number}</li>
         ))}
-        </ul>
+        </ul>   
     </div>
   )
   
